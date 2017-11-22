aebae4f8d548cb11933f149c26819a83243ea0f6
Boss-555 <33842434+Boss-555@users.noreply.github.com>
2017-11-22 21:58:40 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int fn = 1;
+            for (int i = 1; i <= n; ++i)
+                fn *= i;
+            Console.WriteLine("{0}! = {1}", n, fn);
+            Console.ReadLine(); 
+        }
+    }
+}
