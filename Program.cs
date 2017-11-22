d4da1e3fcbf2a11360caefdd8753929d2786d73f
Boss-555 <33842434+Boss-555@users.noreply.github.com>
2017-11-22 23:31:08 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Vuvedete a: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Vuvedete b: ");
+            int b = int.Parse(Console.ReadLine());
+            Swap(ref a, ref b);
+            Console.WriteLine("a = {0}, b = {1}", a, b);
+        }
+        static void Swap(ref int a, ref int b) 
+        {
+             int c = a;
+             a = b;
+             b = c;
+            
+
+
+
+
+
+
+           
+
+        }
+    }
+}
