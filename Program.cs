9a89515c4fe0c545f0842262d9d0944c1593721c
Boss-555 <33842434+Boss-555@users.noreply.github.com>
2017-11-27 02:09:17 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new int[100];
+            var r = new Random(0);
+            for (int i = 0; i < arr.Length; ++i)
+                arr[i] = r.Next(1, 101);
+
+            for (int i = 0; i < 100; i++)
+                Console.WriteLine("{0}:{1}", i +1, arr[i]); 
+
+            Console.ReadLine(); 
+
+        }
+    }
+}
