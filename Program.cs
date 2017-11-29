ea550c779299c7469efe35c705de998ac0b9d833
Boss-555 <33842434+Boss-555@users.noreply.github.com>
2017-11-29 23:26:40 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Дума:");
+                string s = Console.ReadLine();
+                bool isPalindrome = true;
+                for (int i = 0; i < s.Length / 2; i++)
+                {
+                    if (s[i] != s[s.Length - 1 - i])
+                    {
+                        isPalindrome = false;
+                        break;
+                    }
+                }
+                if (isPalindrome)
+                {
+                    Console.WriteLine("Палиндром!");
+                }
+                else
+                    Console.WriteLine("Думата не е палиндром.");
+                break;
+            }
+            Console.WriteLine("Довиждане!");
+
+                    
+                       
+        }
+    }
+}
