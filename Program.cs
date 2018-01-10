3ff5da07d07d037f0549cf76bc8e60d756957f36
Boss-555 <33842434+Boss-555@users.noreply.github.com>
2018-01-10 15:13:27 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input numerical mark:");
+            int num = int.Parse(Console.ReadLine());
+            switch (num)
+            {
+                case 10:
+                    Console.WriteLine("ten");
+                    break;
+                case 9:
+                    Console.Write("Vuvedi a:");
+                    int a = int.Parse(Console.ReadLine());
+                    Console.WriteLine(a);
+                    break;
+                default:
+                    Console.WriteLine("RAZOR");
+                    break;
+            }
+            Console.ReadLine();
+
+            
+        }
+    }
+}
