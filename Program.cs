82eae7eff74fc686b41806e76e5902f2e56ab454
Boss-555 <33842434+Boss-555@users.noreply.github.com>
2018-01-15 13:15:39 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 2, 3, 5, 7, 11, 13, 17, 19 };
+            foreach(int i in numbers)
+            {
+                Console.Write(" " + i);
+            }
+            Console.WriteLine();
+            String[] towns = { "Sofia", "Plovdiv", "Varna", "Burgas" };
+            foreach (String town in towns)
+            {
+                Console.Write(" " + town);
+            }
+            Console.ReadLine();
+        }
+    }
+}
