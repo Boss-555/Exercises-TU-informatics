f2cfdf18433d17362af5ddbd749092b53f44c50d
Boss-555 <33842434+Boss-555@users.noreply.github.com>
2018-01-25 17:51:12 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 33;
+
+            if(x == 8)
+            {
+                Console.WriteLine("Value of x is 8"); 
+
+            } 
+            else if (x == 18)
+            {
+                Console.WriteLine("Value of x is 18");
+            } 
+            else if (x == 33)
+            {
+                Console.WriteLine("Value of x is 33");
+            } 
+            else
+            {
+                Console.WriteLine("No match"); 
+            }
+        }
+    }
+}
