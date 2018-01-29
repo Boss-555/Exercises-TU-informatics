30739ea478dde5124b914729e09f5edccfa5f96a
Boss-555 <33842434+Boss-555@users.noreply.github.com>
2018-01-29 13:21:29 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp21
+{
+    class Program
+    { 
+        static int Sum(int x , int y)
+        {
+            return x + y;
+        } 
+        static void Main(string[] args)
+        {
+            int res = Sum(11, 42);
+            Console.WriteLine(res);
+        
+    
+        }
+    }
+}
