60f7bb5c5faff8a192de3b89586101eed407985b
Boss-555 <33842434+Boss-555@users.noreply.github.com>
2018-02-01 20:21:37 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter count of elements for Fibonacci: ");
+            int n = int.Parse(Console.ReadLine());
+            int a = 0;
+            int b = 1;
+            for(int i = 2; i < n; i++)
+            {
+                int temp = a;
+                a = b;
+                b = temp + b;
+                
+            }
+            Console.WriteLine("Fib{0} is {1}", n, b);
+            
+
+
+            
+        }
+    }
+}
