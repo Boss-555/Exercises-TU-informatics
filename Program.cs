f98a4113b1bf168b6ccad71773b28b6661c36b74
Boss-555 <33842434+Boss-555@users.noreply.github.com>
2018-02-03 15:40:05 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22
+{
+    class Program
+    {
+        static int Area(int h, int w)
+        {
+            return h * w;
+        } 
+        static void Main(string[] args)
+        {
+            int res = Area(w: 5, h: 8);
+            Console.WriteLine(res);
+        }
+    }
+}
