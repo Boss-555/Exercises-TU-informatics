bb0480aa928949b62d075bb230f0896f2a15cdfc
Boss-555 <33842434+Boss-555@users.noreply.github.com>
2018-02-03 15:35:34 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hanoi
+{
+    class HanTowers
+    { 
+        static void Hanoi(int n, int a, int b, int c)
+        {
+            if(n>0)
+            {
+                Hanoi(n - 1, a, c, b);
+                Console.WriteLine("Move to disc {0} to {1}", a, c);
+                Hanoi(n - 1, b, a, c);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Брой дискове: ");
+            int n = int.Parse(Console.ReadLine());
+            int a = 1;
+            int b = 2;
+            int c = 3;
+            Hanoi(n, a, b, c);
+        }
+    }
+}
